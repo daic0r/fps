160d9df4c1016617ee4f738bd6dfb42f5d9ee464
daic0r <daic0r.dev@gmail.com>
2024-07-06 13:28:40 +0200

    Own color type

--- src/rendering/color.h
+++ src/rendering/color.h
@@ -6,6 +6,13 @@ namespace fps::rendering {
       unsigned char r, g, b, a;
 
       auto operator<=> (Color const&) const = default;
+
+      Color operator*(float rhs) const noexcept {
+         return Color{ static_cast<unsigned char>(r * rhs),
+                       static_cast<unsigned char>(g * rhs),
+                       static_cast<unsigned char>(b * rhs),
+                       static_cast<unsigned char>(a * rhs) };
+      }
    };
 }
 
