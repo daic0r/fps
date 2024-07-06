2c497fd85110aeebab143c0e94d4ec9701e53583
daic0r <daic0r.dev@gmail.com>
2024-07-06 13:27:20 +0200

    Fixed rotation matrices; Implemented inversion

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -6,6 +6,45 @@
 #include "utils.h"
 
 namespace fps::math {
+
+   namespace detail {
+      template<typename NumericType>
+      struct mat3x3 {
+         std::array<std::array<NumericType, 3>, 3> data_;
+         
+         constexpr NumericType determinant() const noexcept {
+            return data_[0][0] * (data_[1][1] * data_[2][2] - data_[1][2] * data_[2][1]) +
+               data_[0][1] * (data_[1][2] * data_[2][0] - data_[1][0] * data_[2][2]) +
+               data_[0][2] * (data_[1][0] * data_[2][1] - data_[1][1] * data_[2][0]);
+         }
+
+         constexpr mat3x3 invert() const noexcept {
+            const auto d = determinant();
+            if (test_equal(d, 0.0f, .00001f)) {
+               return mat3x3{};
+            }
+            const auto invdet = NumericType{1} / d;
+            return mat3x3{
+               std::array<NumericType, 3>{
+                  (data_[1][1] * data_[2][2] - data_[1][2] * data_[2][1]) * invdet,
+                  (data_[0][2] * data_[2][1] - data_[0][1] * data_[2][2]) * invdet,
+                  (data_[0][1] * data_[1][2] - data_[0][2] * data_[1][1]) * invdet
+               },
+               std::array<NumericType, 3>{
+                  (data_[1][2] * data_[2][0] - data_[1][0] * data_[2][2]) * invdet,
+                  (data_[0][0] * data_[2][2] - data_[0][2] * data_[2][0]) * invdet,
+                  (data_[0][2] * data_[1][0] - data_[0][0] * data_[1][2]) * invdet
+               },
+               std::array<NumericType, 3>{
+                  (data_[1][0] * data_[2][1] - data_[1][1] * data_[2][0]) * invdet,
+                  (data_[0][1] * data_[2][0] - data_[0][0] * data_[2][1]) * invdet,
+                  (data_[0][0] * data_[1][1] - data_[0][1] * data_[1][0]) * invdet
+               }
+            };
+         }
+      };
+   }
+
    template<typename NumericType>
    class basic_matrix {
       static constexpr inline auto Zero = NumericType{ 0 };
@@ -56,6 +95,33 @@ namespace fps::math {
          return result;
       }
 
+      constexpr basic_matrix transpose() const noexcept {
+         basic_matrix result{};
+         for (std::size_t i = 0; i < 4; ++i) {
+            for (std::size_t j = 0; j < 4; ++j) {
+               result[i,j] = result[j,i];
+            }
+         }
+         return result;
+      }
+
+      constexpr basic_matrix invert() const noexcept {
+         // https://math.stackexchange.com/questions/152462/inverse-of-transformation-matrix
+         const auto inv_top_left3x3 = detail::mat3x3<NumericType>{
+            std::array<NumericType, 3>{m_data[0][0], m_data[0][1], m_data[0][2]},
+            std::array<NumericType, 3>{m_data[1][0], m_data[1][1], m_data[1][2]},
+            std::array<NumericType, 3>{m_data[2][0], m_data[2][1], m_data[2][2]}
+         }.invert();
+         const auto transl = vec3<NumericType>{m_data[0][3], m_data[1][3], m_data[2][3]};
+         const auto inv_transl = inv_top_left3x3 * transl;
+         return basic_matrix{
+            inv_top_left3x3.data_[0][0], inv_top_left3x3.data_[0][1], inv_top_left3x3.data_[0][2], -inv_transl[0],
+            inv_top_left3x3.data_[1][0], inv_top_left3x3.data_[1][1], inv_top_left3x3.data_[1][2], -inv_transl[1],
+            inv_top_left3x3.data_[2][0], inv_top_left3x3.data_[2][1], inv_top_left3x3.data_[2][2], -inv_transl[2],
+            Zero, Zero, Zero, One
+         };
+      }
+
       static constexpr basic_matrix perspective(NumericType fovV, NumericType fAspectRatio, NumericType fNear, NumericType fFar) {
          const auto fov2 = fovV * Half;
          const auto denom = fNear - fFar;
@@ -88,23 +154,23 @@ namespace fps::math {
          const auto s = sinf(angle);
          const auto c = cosf(angle);
          return basic_matrix{ One, Zero, Zero, Zero,
-                        Zero, c, s, Zero,
-                        Zero, -s, c, Zero,
+                        Zero, c, -s, Zero,
+                        Zero, s, c, Zero,
                         Zero, Zero, Zero, One };
       }
       static constexpr basic_matrix rotate_y(NumericType angle) noexcept {
          const auto s = sinf(angle);
          const auto c = cosf(angle);
-         return basic_matrix{ c, Zero, -s, Zero,
+         return basic_matrix{ c, Zero, s, Zero,
                         Zero, One, Zero, Zero,
-                        s, Zero, c, Zero,
+                        -s, Zero, c, Zero,
                         Zero, Zero, Zero, One };
       }
       static constexpr basic_matrix rotate_z(NumericType angle) noexcept {
          const auto s = sinf(angle);
          const auto c = cosf(angle);
-         return basic_matrix{ c, s, Zero, Zero,
-                        -s, c, Zero, Zero,
+         return basic_matrix{ c, -s, Zero, Zero,
+                        s, c, Zero, Zero,
                         Zero, Zero, One, Zero,
                         Zero, Zero, Zero, One };
       }
